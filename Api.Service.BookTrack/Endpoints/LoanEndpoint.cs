0525c645ae85538b41dc19b7a8cfbe40901efaf5
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2023-07-16 23:40:03 -0300

    feat: cria loan service e loan endpoint

---
+++ Api.Service.BookTrack/Endpoints/LoanEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using Api.Service.BookTrack.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Service.BookTrack.Endpoints
+{
+    internal class LoanEndpoint
+    {
+        private BookService BookService { get; }
+
+        private UserService UserService { get; }
+
+        private LoanService LoanService { get; }
+
+        public LoanEndpoint(BookService bookService, UserService userService, LoanService loanService)
+        {
+            BookService = bookService;
+            UserService = userService;
+            LoanService = loanService;
+        }
+    }
+}
---
+++ Api.Service.BookTrack/Services/LoanService.cs
@@ -0,0 +1,11 @@
+﻿using Api.Service.BookTrack.Ioc;
+
+namespace Api.Service.BookTrack.Services
+{
+    [Ioc(Interface = typeof(LoanService))]
+    public class LoanService
+    {
+        
+
+    }
+}
