fb22e103db833cbb51b1c481555a446d0e419f17
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2023-07-17 22:32:14 -0300

    feat: cira endpoint para solicitar um emprestimo

--- Api.Service.BookTrack/Endpoints/LoanEndpoint.cs
+++ Api.Service.BookTrack/Endpoints/LoanEndpoint.cs
@@ -1,4 +1,9 @@
 ﻿using Api.Service.BookTrack.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +25,14 @@ namespace Api.Service.BookTrack.Endpoints
             BookService = bookService;
             UserService = userService;
             LoanService = loanService;
+        }
+
+        [FunctionName("RequestLoan")]
+        public async Task<IActionResult> RequestLoan(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "v1/request/loan")] HttpRequest req,
+            ILogger log)
+        {
+            return new OkResult();
         }
     }
 }
